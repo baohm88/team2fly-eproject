8542d0aa8fbbb55e582a17edb977c6dd12dd4845
Bao Ha <baohm88@gmail.com>
2024-09-10 14:40:18 +0700

    added HomePage, ProductsPage, RootLayout, ErrorPage

---
+++ frontend/nike-react-php/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header({ children }) {
+  return (
+    <header>
+      <nav>{children}</nav>
+    </header>
+  );
+}
---
+++ frontend/nike-react-php/src/pages/HomePage.jsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <h1>Nike Home Page</h1>;
+}
