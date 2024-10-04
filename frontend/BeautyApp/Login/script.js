8b650ed1bce42d0870464caefb5ba467c1a1407e
TongM28 <137702324+TongM28@users.noreply.github.com>
2024-10-04 13:59:31 +0700

    login and register

---
+++ frontend/BeautyApp/Login/script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+    hamburger.classList.toggle('active');
+});
